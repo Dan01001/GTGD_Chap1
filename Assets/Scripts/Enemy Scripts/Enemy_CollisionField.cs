0a43458ca076785c92c47693d2e72e30f9ebff50
Dan01001 <dmmajewski@gmail.com>
2016-10-30 22:02:53 -0400

    through lesson 86

---
+++ Assets/Scripts/Enemy Scripts/Enemy_CollisionField.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace S3
+{
+    public class Enemy_CollisionField : MonoBehaviour {
+
+        private Enemy_Master enemyMaster;
+        private Rigidbody rigidBodyStrikingMe;
+        private int damageToApply;
+        public float massRequirement = 50;
+        public float speedRequirment = 5;
+        private float damageFactor = 0.1f;
+
+        void OnEnable()
+        {
+            SetIntialReferences();
+            enemyMaster.EventEnemyDie += DisableThis;
+        }
+        
+        void OnDisable()
+        {
+            enemyMaster.EventEnemyDie -= DisableThis;
+        }
+
+        void SetIntialReferences()
+        {
+            enemyMaster = transform.root.GetComponent<Enemy_Master>();
+        }
+
+        void OnTriggerEnter(Collider other)
+        {
+            if(other.GetComponent<Rigidbody>() != null)
+            {
+                rigidBodyStrikingMe = other.GetComponent<Rigidbody>();
+
+                if (rigidBodyStrikingMe.mass >= massRequirement && rigidBodyStrikingMe.velocity.sqrMagnitude > speedRequirment * speedRequirment)
+                {
+                    damageToApply = (int)(damageFactor * rigidBodyStrikingMe.mass * rigidBodyStrikingMe.velocity.magnitude);
+                    enemyMaster.CallEventEnemyDeductHealth(damageToApply);
+                }
+            }
+        }
+
+        void DisableThis()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
